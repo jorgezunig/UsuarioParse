6f04c4f258dfe83081cc10a4f21a93d87d4fd264
LENOVO <jorge.z.delacruz@gmail.com>
2016-03-29 12:10:24 -0600

    Creación de Clase y Layout "Bienvenida" Se aregan Strings de Layout "bienvenida"

--- ParseStarterProject/src/main/java/com/parse/starter/Bienvenida.java
+++ ParseStarterProject/src/main/java/com/parse/starter/Bienvenida.java
@@ -1,5 +1,45 @@
 package com.parse.starter;
 
 
-public class Bienvenida {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
+
+public class Bienvenida extends Activity {
+    Button logout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Obteniendo la Vista XML
+        setContentView(R.layout.bienvenida);
+
+        // Localizando elementos de la Vista xml
+        TextView txtuser = (TextView) findViewById(R.id.txtuser);
+        logout = (Button) findViewById(R.id.logout);
+
+        // Obetniendo usuario actual
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        // Conversion de usuario actual a String
+        String struser = currentUser.getUsername().toString();
+
+        // Estableciendo String de usuario a TextView
+        txtuser.setText(" " + struser);
+
+        // Evento de Boton Fin de Seseion
+        logout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                // Fin de Sesion
+                ParseUser.logOut();
+                finish();
+            }
+        });
+
+
+    }
 }
